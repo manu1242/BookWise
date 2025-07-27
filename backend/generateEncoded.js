568b3d8d099dda638d1026bc16b4d39e369f7a32
manu1242 <manoharyalla77@gmail.com>
2025-07-27 23:23:33 +0530

    api changes

--- backend/generateEncoded.js
+++ backend/generateEncoded.js
@@ -4,8 +4,8 @@ const encoded = Buffer.from(JSON.stringify({ email })).toString("base64");
 console.log("Encoded Base64 string:\n", encoded);
 
 
+const approvalLink = `${process.env.SERVER_URL}api/auth/approve-admin?data=${encodedData}`;
 
-const approvalLink = `https://book-wise-navy.vercel.app/api/auth/approve-admin?data=${encodedData}`;
 
 // Include `approvalLink` in the email body
 

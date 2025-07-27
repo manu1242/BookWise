b1c12dd2ae959f635b32d3af73ab16a8e79cc10c
manu1242 <manoharyalla77@gmail.com>
2025-07-27 22:07:34 +0530

    Cors change to allow all

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true, // Allow all origins
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 

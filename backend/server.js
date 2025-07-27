14a211f5e1210311823e1d93e98e096e0fa966e9
manu1242 <manoharyalla77@gmail.com>
2025-07-27 22:05:54 +0530

    changes in server

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
-    
+    origin: "http://localhost:5173",
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })

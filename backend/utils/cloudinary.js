ce6dd8ab41e2b8d1d7343db11caf73866ed17ab5
manu1242 <manoharyalla77@gmail.com>
2025-07-27 21:49:43 +0530

    MAKING PACKAGE CHANGES

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,13 +1,9 @@
-const cloudinary = require("cloudinary").v2;
-const dotenv = require("dotenv");
-dotenv.config();
+const { v2: cloudinary } = require('cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-console.log("Cloudinary Connected successFully....");
 
 module.exports = cloudinary;
